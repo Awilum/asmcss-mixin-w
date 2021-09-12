64d47e2d79e308d6035a8dc2cf3b46312675c012
--Explosion-- <explosion.scratch@gmail.com>
2021-09-12 14:32:14 -0500

    Add `screen`, `min`, `max`, `auto`, and `full`

--- dist/js/asmcss-mixin-w.js
+++ dist/js/asmcss-mixin-w.js
@@ -1,5 +1,17 @@
 AssemblerCSS.registerMixin("w", function (settings, fraction) {
     if (!/[0-9]+\/[0-9]+/.test(fraction){
+        var otherOpts = {
+            screen: "100vw",
+            min: "min-content",
+            max: "max-content",
+            auto: "auto",
+            full: "100%"
+        }
+        //Not sure if toLowerCase and trim() are nessecary but why not
+        var key = fraction.toLowerCase().trim()
+        if (otherOpts[key]) {
+            fraction = otherOpts[key];
+        }
         //If we can't divide it just return the original thing (Like `w:120px`)
         return `w: ${fraction};`;
     }
