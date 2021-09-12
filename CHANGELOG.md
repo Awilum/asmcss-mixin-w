1a4d5f52c6176e96c297d888342b6ffd8ac3e0e8
Awilum <sergey.romanenko@flextype.org>
2021-09-12 19:18:19 +0300

    asmcss-mixin-w v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+<a name="1.0.1"></a>
+# [1.0.1](https://github.com/Awilum/asmcss-mixin-w/compare/v1.0.0...v1.0.1) (2020-09-12)
+* Added `gulp-uglify` to minify build
+
 <a name="1.0.0"></a>
 # [1.0.0](https://github.com/Awilum/asmcss-mixin-w) (2020-09-12)
 * Initial Release
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require("gulp")
 const jsValidate = require("gulp-jsvalidate")
 const rename = require("gulp-rename")
-const jsmin = require('gulp-jsmin');
+const uglify = require('gulp-uglify')
 const size = require("gulp-size")
 
 const files = ["src/asmcss-mixin-w.js"]
@@ -9,7 +9,10 @@ const files = ["src/asmcss-mixin-w.js"]
 const buildTask = () =>
 	gulp
 		.src(files)
-		.pipe(jsmin())
+        .pipe(rename("asmcss-mixin-w.js"))
+		.pipe(gulp.dest("dist/js"))
+		.pipe(size({ showFiles: true }))
+		.pipe(uglify())
 		.pipe(rename("asmcss-mixin-w.min.js"))
 		.pipe(gulp.dest("dist/js"))
 		.pipe(size({ showFiles: true }))
