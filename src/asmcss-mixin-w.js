7902c6a0684845e2b18c23ce551aec6d8fdd489b
Awilum <sergey.romanenko@flextype.org>
2021-09-12 22:59:49 +0300

    small fixes and rebuild dist

--- src/asmcss-mixin-w.js
+++ src/asmcss-mixin-w.js
@@ -1,5 +1,5 @@
 AssemblerCSS.registerMixin("w", function (settings, fraction) {
-    if (!/[0-9]+\/[0-9]+/.test(fraction){
+    if (!/[0-9]+\/[0-9]+/.test(fraction)) {
         var otherOpts = {
             screen: "100vw",
             min: "min-content",
@@ -7,14 +7,16 @@ AssemblerCSS.registerMixin("w", function (settings, fraction) {
             auto: "auto",
             full: "100%"
         }
-        //Not sure if toLowerCase and trim() are nessecary but why not
-        var key = fraction.toLowerCase().trim()
+
+        var key = fraction.toLowerCase().trim();
+
         if (otherOpts[key]) {
             fraction = otherOpts[key];
         }
-        //If we can't divide it just return the original thing (Like `w:120px`)
+        
         return `w: ${fraction};`;
     }
-    //If it's a fraction we can just do this:
+
+
     return `w: ${(fraction.split("/")[0] / fraction.split("/")[1]) * 100}%;`
 });
